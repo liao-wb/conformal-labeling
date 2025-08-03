9c9032388690869e726a4b6b4a8a43945930387b
33798 <3379854453@qq.com>
2025-08-03 16:54:07 +0800

    msg

--- image_classification/util.py
+++ image_classification/util.py
@@ -8,7 +8,7 @@ import torchvision.models as models
 import torch.backends.cudnn as cudnn
 import pandas as pd
 
-import configs
+import config
 
 
 def set_seed(seed):
@@ -24,17 +24,28 @@ def set_seed(seed):
     return seed
 
 
-def build_dataset(dataset_name, root, batch_size=256, num_workers=8):
+def build_dataset(dataset_name, device, batch_size=256, num_workers=8):
     if dataset_name == "cifar10":
-        testset = datasets.CIFAR10(root=configs.DATA_CIFAR10_ROOT, train=False, transform=configs.TRANSFORM_CIFAR10_TEST)
+        testset = datasets.CIFAR10(root=config.DATA_CIFAR10_ROOT, train=False, transform=config.TRANSFORM_CIFAR10_TEST)
         # testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size, num_workers=num_workers,
                                                  # shuffle=True)
-        cal_num = configs.CIFAR10_CALNUM
-        test_num = configs.CIFAR10_TESTNUM
+        cal_num = config.CIFAR10_CALNUM
+        test_num = config.CIFAR10_TESTNUM
         calibset, testset = torch.utils.data.random_split(testset, [cal_num, test_num])
         calibloader = torch.utils.data.DataLoader(dataset=calibset, batch_size=batch_size, num_workers=num_workers,
                                                   shuffle=True)
         testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size, num_workers=num_workers,
                                                  shuffle=True)
 
-    return calibloader, testloader
+    return calibloader.to(device), testloader.to(device)
+
+
+def build_model(model_name, dataset_name, device):
+    if dataset_name == "cifar10":
+        from model.cifar10.resnet import ResNet18
+        model = ResNet18()
+        checkpoint = torch.load(config.RESNET18_CIFAR10_ROOT)
+        model.load_state_dict({k.replace('module.', ''): v for k, v in checkpoint['net'].items()})
+
+    model.eval()
+    return model.to(device)
