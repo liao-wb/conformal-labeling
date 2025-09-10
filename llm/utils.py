01674f655112b51acd87dbe31d97b63dddedf5be
AIAI333 <12312107@mail.sustech.edu.cn>
2025-09-10 21:40:57 +0800

    Update

--- llm/utils.py
+++ llm/utils.py
@@ -23,9 +23,17 @@ def get_dataset(args):
         }
     elif args.dataset == "mmlu":
         label_list = ['A', 'B', 'C', 'D']
-        test_dataset = load_from_disk("/mnt/sharedata/ssd_large/common/datasets/mmlu/all/test-00000-of-00001.parquet")
-        val_dataset = load_from_disk("/mnt/sharedata/ssd_large/common/datasets/mmlu/all/validation-00000-of-00001.parquet")
-        dataset = concatenate_datasets([val_dataset, test_dataset])
+        #test_dataset = load_from_disk("/mnt/sharedata/ssd_large/common/datasets/mmlu/all/test-00000-of-00001.parquet")
+        #val_dataset = load_from_disk("/mnt/sharedata/ssd_large/common/datasets/mmlu/all/validation-00000-of-00001.parquet")
+        #dataset = concatenate_datasets([val_dataset, test_dataset])
+
+        full_dataset = load_dataset(
+            "parquet",
+            data_files={
+                "test": "/mnt/sharedata/ssd_large/common/datasets/mmlu/all/test-00000-of-00001.parquet",
+                "validation": "/mnt/sharedata/ssd_large/common/datasets/mmlu/all/validation-00000-of-00001.parquet"}
+        )
+        dataset = concatenate_datasets([full_dataset["validation"], full_dataset["test"]])
 
         reformat = lambda x: {
             'question': x['question'],
