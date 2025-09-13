73599a483b3a0c316d3301dcf038db9311a5f5d3
AIAI333 <12312107@mail.sustech.edu.cn>
2025-09-13 21:58:12 +0800

    Update

--- rlhf_labeling/utils.py
+++ rlhf_labeling/utils.py
@@ -11,36 +11,34 @@ def fix_note(example):
     return example
 
 def get_dataset(args):
-    dataset_name = args.dataset
-
-    if args.dataset == "tl;dr":
+    if args.dataset == "tldr":
         label_list = ['A', 'B']
 
         """batch_files = [
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch3.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch4.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch5.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch6.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch7.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch8.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch9.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch10.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch11.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch12.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch13.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch14.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch15.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch16.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch17.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch18.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch19.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch20.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch22.json"
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch3.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch4.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch5.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch6.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch7.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch8.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch9.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch10.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch11.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch12.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch13.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch14.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch15.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch16.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch17.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch18.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch19.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch20.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch22.json"
         ]
 """
         batch_files = [
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch3.json",
-            "/mnt/e/Users/27859/PycharmProjects/select_reliable_predictions/data/comparisons/batch22.json"
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch3.json",
+            "/mnt/sharedata/hdd/users/huanghp/comparisons/batch22.json"
         ]
 
         # Load each dataset individually
