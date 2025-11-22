148f84d89564b470b9aa0fb6582e86dccd9e1b0c
AIAI333 <12312107@mail.sustech.edu.cn>
2025-11-22 21:02:45 +0800

    Update

--- image_classification/main.py
+++ image_classification/main.py
@@ -74,6 +74,58 @@ elif args.dataset == "imagenetc1":
         root=val_dir,
         transform=val_tf
     )
+elif args.dataset == "imagenetc2":
+    val_dir = "/mnt/sharedata/ssd_small/common/datasets/imagenet-corruption/brightness/2"
+    val_tf = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    test_dataset = torchvision.datasets.ImageFolder(
+        root=val_dir,
+        transform=val_tf
+    )
+elif args.dataset == "imagenetc3":
+    val_dir = "/mnt/sharedata/ssd_small/common/datasets/imagenet-corruption/brightness/3"
+    val_tf = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    test_dataset = torchvision.datasets.ImageFolder(
+        root=val_dir,
+        transform=val_tf
+    )
+elif args.dataset == "imagenetc4":
+    val_dir = "/mnt/sharedata/ssd_small/common/datasets/imagenet-corruption/brightness/4"
+    val_tf = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    test_dataset = torchvision.datasets.ImageFolder(
+        root=val_dir,
+        transform=val_tf
+    )
+elif args.dataset == "imagenetc5":
+    val_dir = "/mnt/sharedata/ssd_small/common/datasets/imagenet-corruption/brightness/5"
+    val_tf = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    test_dataset = torchvision.datasets.ImageFolder(
+        root=val_dir,
+        transform=val_tf
+    )
 else:
     raise NotImplementedError(f"Dataset {args.dataset} not supported")
 
